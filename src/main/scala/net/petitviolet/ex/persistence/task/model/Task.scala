b707cfac77811e3e63b2578f60e4ce5713d7c392
petitviolet <violethero0820@gmail.com>
2016-08-19 10:39:33 +0900

    task with identification

--- src/main/scala/net/petitviolet/ex/persistence/task/model/Task.scala
+++ src/main/scala/net/petitviolet/ex/persistence/task/model/Task.scala
@@ -1,17 +1,28 @@
 package net.petitviolet.ex.persistence.task.model
 
+import java.util.UUID
+
 import spray.json._
 
-case class Task(title: TaskTitle, state: TaskState = TaskState.Todo)
+import scala.util.Random
 
-case class TaskTitle(value: String)
+case class Task(id: TaskId, title: TaskTitle, state: TaskState = TaskState.Todo)
+
+case class TaskId(value: String) extends AnyVal
+case class TaskTitle(value: String) extends AnyVal
 
 sealed abstract class TaskState(val value: Int) extends Serializable
 
+object TaskId {
+  def create: TaskId = {
+    apply(UUID.randomUUID().toString.take(5))
+  }
+}
 object TaskState {
   private val values = Completed :: Todo :: Nil
 
-  def from(n: Int): TaskState = values.find(_.value == n).get
+  //  def from(n: Int): TaskState = values.find(_.value == n).get
+  def from: Int => TaskState = n => values.find(_.value == n).get
 
   case object Completed extends TaskState(1)
   case object Todo extends TaskState(0)
@@ -20,14 +31,15 @@ object TaskState {
 object TaskJsonSupport extends DefaultJsonProtocol {
   implicit val taskJsonFormat: RootJsonFormat[Task] = new RootJsonFormat[Task] {
     override def write(obj: Task): JsValue = JsObject(
+      "id" -> JsString(obj.id.value),
       "title" -> JsString(obj.title.value),
       "state" -> JsNumber(obj.state.value)
     )
 
     override def read(json: JsValue): Task =
       json.asJsObject.getFields("title", "state") match {
-        case Seq(JsString(title), JsNumber(state)) =>
-          Task(TaskTitle(title), TaskState.from(state.toInt))
+        case Seq(JsString(id), JsString(title), JsNumber(state)) =>
+          Task(TaskId(id), TaskTitle(title), TaskState.from(state.toInt))
         case _ => throw new DeserializationException("Task")
       }
   }
