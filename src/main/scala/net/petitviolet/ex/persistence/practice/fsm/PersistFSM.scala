20b2d0cc7875722bc48baf695381ea94543b6b50
petitviolet <violethero0820@gmail.com>
2016-08-21 21:23:16 +0900

    updated PersistentFSM sample

--- src/main/scala/net/petitviolet/ex/persistence/practice/fsm/PersistFSM.scala
+++ src/main/scala/net/petitviolet/ex/persistence/practice/fsm/PersistFSM.scala
@@ -16,9 +16,10 @@ private object ForFSM {
   case class RemoveItem(item: Item) extends ShoppingEvent
   case object Purchase extends ShoppingEvent
   case object Leave extends ShoppingEvent
-  case object Save extends ShoppingEvent
 
-  case class ShoppingData private[ShoppingData] (items: Set[Item])
+  case class ShoppingData private[ShoppingData] (items: Set[Item]) {
+    def price: Int = items.map(_.price).sum
+  }
   object ShoppingData {
     val empty = ShoppingData(Set.empty)
   }
@@ -37,7 +38,7 @@ private class Customer(implicit val domainEventClassTag: ClassTag[ShoppingEvent]
   override def persistenceId: String = "example-persistence-FSM"
 
   private def purchase(data: ShoppingData): Unit =
-    println(s"price => ${data.items.map(_.price).sum}")
+    println(s"price => ${data.price}")
 
   override def applyEvent(domainEvent: ShoppingEvent, currentData: ShoppingData): ShoppingData = {
     log.info(s"\n***applyEvent: $domainEvent, $currentData, state: $stateName")
@@ -51,40 +52,67 @@ private class Customer(implicit val domainEventClassTag: ClassTag[ShoppingEvent]
         ShoppingData.empty
       case Leave =>
         ShoppingData.empty
-      case Save =>
-        saveStateSnapshot()
-        currentData
     }
   }
-
-  // initial state and data
-  startWith(Looking, ShoppingData.empty)
 
   override def onRecoveryCompleted(): Unit = {
     log.info(s"recovery completed: $stateData")
   }
 
+  // initial state and data
+  startWith(Looking, ShoppingData.empty)
+
   // extended `goto` with logging
   private def gotoLogging(state: ShoppingState) = {
-    log.debug(s"\n***state => $state, current: $stateName")
+    log.info(s"\ngoto $stateName => $state")
     super.goto(state)
   }
 
+  // extended `stay` with logging
+  private def stayLogging = {
+    log.info(s"\nstay $stateName")
+    super.stay
+  }
+
   when(Looking) {
-    case Event(Save, _)             => stay applying Save
-    case Event(addItem: AddItem, _) => gotoLogging(Shopping) applying addItem
+    case Event(addItem: AddItem, _) =>
+      // start shopping with adding an item
+      gotoLogging(Shopping) applying addItem andThen {
+        case afterAddItem => log.info(s"after => $afterAddItem")
+      }
   }
 
   when(Shopping) {
-    case Event(Save, _)                   => stay applying Save
-    case Event(addItem: AddItem, _)       => stay applying addItem
-    case Event(removeItem: RemoveItem, _) => stay applying removeItem
-    case Event(Purchase, _)               => gotoLogging(Purchased) applying Purchase
+    case Event(addItem: AddItem, _) =>
+      // just add an item
+      stayLogging applying addItem
+
+    case Event(removeItem: RemoveItem, ShoppingData.empty) =>
+      // if currentData is empty, disallowed RemoveItem
+      throw new IllegalStateException("ShoppingCart is Empty")
+    case Event(removeItem @ RemoveItem(item), ShoppingData(items)) if items.size == 1 && (items contains item) =>
+      // if currentData will be removed, goto Looking state
+      gotoLogging(Looking) applying removeItem
+    case Event(removeItem: RemoveItem, _) =>
+      // general behavior, stay and remove
+      stayLogging applying removeItem
+
+    case Event(Purchase, _) =>
+      // go to cashier and having payment
+      gotoLogging(Purchased) applying Purchase
   }
 
   when(Purchased) {
-    case Event(Save, _)  => stay applying Save
-    case Event(Leave, _) => gotoLogging(Looking) applying Leave
+    case Event(Leave, _) =>
+      // leave from the shop
+      gotoLogging(Looking) applying Leave
+  }
+
+  onTransition {
+    case Purchased -> Looking =>
+      // take snapshot when shopping has completed
+      log.info(s"Save SnapShot")
+      saveStateSnapshot()
   }
 
 }
@@ -98,17 +126,17 @@ private object PersistFSMApp extends App {
   val meat = Item("meat", 1000)
 
   actor ! AddItem(milk)
-  actor ! AddItem(meat)
   actor ! RemoveItem(milk)
+  actor ! AddItem(meat)
 
   //  actor ! Purchase
   //  actor ! Leave
-
+  //
   val rand = Item("mystery", Random.nextInt(300))
   actor ! AddItem(rand)
-
-  actor ! Save
-  actor ! AddItem(milk)
+  //
+  //  //  actor ! Save
+  //  actor ! AddItem(milk)
 
   Thread.sleep(1000)
   system.terminate()
